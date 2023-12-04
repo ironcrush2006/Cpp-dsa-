3e2105821a65479352586a2ca69e07cf5c78891c
Aditya NV <adityanv4@gmail.com>
2023-12-04 18:55:14 +0530

    singlely linkedlist

---
+++ linkedlist.cpp
@@ -0,0 +1,34 @@
+#include<iostream> 
+//user-defined datatype class
+class node{ 
+    public:
+        int data; 
+        node* next;
+}; 
+void printList(node*n){  
+    //loop to print the values
+    while(n!=NULL){ 
+         std::cout<<n->data<<std::endl; //values stored in the int data
+        n=n->next;
+         std::cout<<n->next<<std::endl;//memory address of node*next
+    }
+}
+int main(){ 
+      node*head=new node();  
+      node*second=new node(); 
+      node*third=new node(); 
+      //assign the values to nodes
+      head->data=1; 
+      head->next=second; 
+      second->data=23; 
+      second->next=third; 
+      third->data=45; 
+      third->next=NULL; 
+      //callback the function to print 
+      printList(head);  
+      delete head; 
+      delete second; 
+      delete third;
+      return 0;
+
+}
